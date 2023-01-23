a7c7744a4ae165638d74b704828e13f051e434e0
Sunil <snilavarath@gmail.com>
2023-01-23 13:07:45 -0500

    win screen fix

--- client/src/components/WinScreen/WinScreen2.js
+++ client/src/components/WinScreen/WinScreen2.js
@@ -3,22 +3,26 @@ import "./WinScreen.css";
 
 function WinScreen({ refreshPage, attempts, seconds }) {
   return (
-    <div className="modal-Background">
-      <div className="modalContainer">
-        <div className="title">
-          <h1>Congratulations, You Win!</h1>
-        </div>
-        <br></br>
-        <div className="-body">
-          <h4>You Guessed Correctly in {10 - attempts} Tries</h4>
-          <h4>in {seconds} Seconds!</h4>
-        </div>
-        <div className="footer">
-          <button onClick={refreshPage}>Play Again</button>
-        </div>
+    <div className="win-modal-Background">
+    <div className="win-modalContainer">
+      <div className="title">
+        <h1>Congratulations, You Win!</h1>
+      </div>
+      <br></br>
+      <div className="body">
+      <p>You Guessed Correctly in:</p><br></br><p> {10 - attempts} Tries
+          And {seconds} Seconds!</p>
+      </div>
+      <div className="footer">
+        <button onClick={refreshPage}>Play Again</button>
       </div>
     </div>
+  </div>
   );
 }
 
 export default WinScreen;
+
+
+{/* <h4>You Guessed Correctly in {10 - attempts} Tries</h4>
+          <h4>in {seconds} Seconds!</h4> */}
